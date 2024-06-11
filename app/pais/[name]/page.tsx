208b3b4a6ff2858f40c0b3c3eb68ac7e5a492645
Everton Espedito <152447787+EvertonEspedito@users.noreply.github.com>
2024-06-11 14:52:16 -0300

    Update page.tsx

--- app/pais/[name]/page.tsx
+++ app/pais/[name]/page.tsx
@@ -73,7 +73,7 @@ export default async function CountryPage({
                 <h3 className="mt-12 mb-4 text-2xl font-semibold text-gray-800">Paises que fazem fronteiras</h3>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  w-full container gap-2">
                     {borderCountries?.map((border)=>(
-                       <CountryCard { ... border}/>
+                       <CountryCard key={border.name}{ ... border}/>
                     ))}
                 </div>
             </section>
